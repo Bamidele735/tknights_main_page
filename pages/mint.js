56b73a9b37e7e247351140e211034aa4bd5e2d94
Wenzel <wenzel.natebus@gmail.com>
2023-01-25 02:03:23 +0100

    faq mint raus

--- pages/mint.js
+++ pages/mint.js
@@ -6,7 +6,6 @@ import styles from "../styles/Landingpage/general.module.css"
 import Footer from '../components/Footer';
 
 import Firstscsc from '@/components/mint/firstsc';
-import Faq from '@/components/mint/faq';
 import Mint from '@/components/mint/mint';
 import Socials from '@/components/main/socials';
  
@@ -23,7 +22,6 @@ export default function Home() {
 
         <Firstscsc />
         <Mint />
-        <Faq />
         <div className={styles.newsletter_mint}>
         <iframe src="https://t4fe34ba5.emailsys1a.net/202/24/6d10d1c7ed/subscribe/form.html?_g=1674515215" frameborder="0" width="100%" height="450"></iframe>
         </div>
